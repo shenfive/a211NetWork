f4b76d8febbc6e7715d5a1894234cac2485a4998
Shen Jun Wu <shenfive@gmail.com>
2020-12-05 17:28:36 +0800

    try error

--- a211NetWork/ViewController.swift
+++ a211NetWork/ViewController.swift
@@ -15,12 +15,23 @@ class ViewController: UIViewController {
         let apiModel = APIModel.share
         
         apiModel.queryRandomUserAlamofire { (data, error) in
+            if error != nil{
+                print(error?.localizedDescription)
+                self.message(message: error?.localizedDescription ?? "")
+                return
+            }
             let json = JSON(data)
             print(json["results"][0]["picture"]["large"].stringValue)
+            self.message(message: json["results"][0]["picture"]["large"].stringValue)
         }
         
     }
-
+    func message(message:String){
+        let alert = UIAlertController(title: "錯誤", message: message, preferredStyle: .alert)
+        let okBut = UIAlertAction.init(title: "我知道了", style: .default, handler: nil)
+        alert.addAction(okBut)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
