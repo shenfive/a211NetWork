de8f1dcf87ec7f209f95a7ef2bc46654898d3105
Shen Jun Wu <shenfive@gmail.com>
2020-12-05 16:54:31 +0800

    init

---
+++ a211NetWork/APIModel.swift
@@ -0,0 +1,16 @@
+//
+//  APIModel.swift
+//  a211NetWork
+//
+//  Created by 申潤五 on 2020/12/5.
+//
+
+import UIKit
+import Alamofire
+
+class APIModel{
+
+    static var share = APIModel()
+    private init(){}
+    
+}
--- a211NetWork/ViewController.swift
+++ a211NetWork/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let apiModel = APIModel.share
     }
 
 
